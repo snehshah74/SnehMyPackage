eedaa699c506976f925d740b3671bea69fe23c38
Sneh Shah <snehshah74@gmail.com>
2024-09-11 17:44:04 +0000

    Initial commit

---
+++ src/SnehMyPackage.jl
@@ -0,0 +1,7 @@
+module SnehMyPackage
+
+export hello
+
+hello() = "Hello, World!"
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using SnehMyPackage
+using Test
+
+# Example test cases
+using Test
+using SnehMyPackage
+
+@testset "Basic Tests" begin
+    @test hello() == "Hello, World!"
+end
+
+3
